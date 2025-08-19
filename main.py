876cca9a3f2e5f47bb094be58b5c8593f49e25b9
T.S.U. - Ing. Carlos H. <carlos.e.herrera.j@gmail.com>
2025-08-19 14:56:14 -0400

    Sync - 2025/08/19 - 14:56
    Sync - 2025/08/19 - 14:56

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 from routes.user import user_router
-app = FastAPI()
+
+app = FastAPI(
+    title='Python - FastAPI - MySQL',
+    openapi_tags=[
+        { 'name': 'users', 'description':'users routers', }, 
+    ],
+    version='1.0.0',
+    description= 'API testing'
+)
 
 app.include_router(user_router)
